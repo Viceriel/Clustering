a3cb9c09078d1ccfeb1f230bca8e2905700ebad8
Viceriel <viceriel@gmail.com>
2018-02-09 21:24:04 +0100

    Loading all images from desired deirectory

--- load.py
+++ load.py
@@ -3,28 +3,30 @@ import numpy as np
 from PIL import Image
 
 def loadImage():
-    filename_queue = tf.train.string_input_producer(["./images/dom1.jpg"])
+    pattern = tf.train.match_filenames_once("./images/*.jpg")
+    filename_queue = tf.train.string_input_producer(pattern)
+    #filename_queue = tf.train.string_input_producer(["./images/dom1.jpg"])
     image_reader = tf.WholeFileReader()
     _, image_file = image_reader.read(filename_queue)
     my_img = tf.image.decode_jpeg(image_file)
 
-    init_op = tf.global_variables_initializer()
+    init_op = tf.local_variables_initializer()
+    image = []
 
     with tf.Session() as sess:
         sess.run(init_op)
         # Start populating the filename queue.
-
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
+        num = len(pattern.eval())
 
-        for i in range(1): #length of your filename list
-            image = my_img.eval() #here is your image Tensor :)
+        for i in range(num): #length of your filename list
+            image.append(my_img.eval()) #here is your image Tensor :)
 
-            print(image.shape)
-            Image.fromarray(np.asarray(image)).show()
+            print(image[i].shape)
+            Image.fromarray(np.asarray(image[i])).show()
 
             coord.request_stop()
             coord.join(threads)
-            sess.close()
 
     return image
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import numpy as np
 from PIL import Image
-import pdb
 import load
+import pdb
 
 # plt.plot([1, 2, 3, 4])
 # plt.show()
-pdb.set_trace()
 res = load.loadImage();
