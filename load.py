f10ba5f7d4382ae6e95e91c484aea950414f9aae
Viceriel <viceriel@gmail.com>
2018-02-11 10:59:15 +0100

    Switch to dataset API

--- load.py
+++ load.py
@@ -1,32 +1,19 @@
 import tensorflow as tf
 import numpy as np
-from PIL import Image
 
-def loadImage():
+def loadImage2(img_path):
+    img_file = tf.read_file(img_path)
+    img_decoded = tf.image.decode_image(img_file)
+
+    img_decoded= tf.image.per_image_standardization(img_decoded)
+    return img_decoded
+
+def getLabels():
     pattern = tf.train.match_filenames_once("./images/*.jpg")
-    filename_queue = tf.train.string_input_producer(pattern)
-    #filename_queue = tf.train.string_input_producer(["./images/dom1.jpg"])
-    image_reader = tf.WholeFileReader()
-    _, image_file = image_reader.read(filename_queue)
-    my_img = tf.image.decode_jpeg(image_file)
-
     init_op = tf.local_variables_initializer()
-    image = []
 
     with tf.Session() as sess:
         sess.run(init_op)
-        # Start populating the filename queue.
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
-        num = len(pattern.eval())
+        labels = pattern.eval()
 
-        for i in range(num): #length of your filename list
-            image.append(my_img.eval()) #here is your image Tensor :)
-
-            print(image[i].shape)
-            Image.fromarray(np.asarray(image[i])).show()
-
-            coord.request_stop()
-            coord.join(threads)
-
-    return image
+    return labels
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from PIL import Image
 import load
-import pdb
 import numpy as np
+from PIL import Image
+import pdb
 
 # plt.plot([1, 2, 3, 4])
 # plt.show()
-res = load.loadImage()
-avg = np.mean(res, dtype=np.float64)
-std = np.std(res, dtype=np.float64)
-res = res - avg
-avg = np.mean(res, dtype=np.float64)
-res = np.divide(res, std)
-std = np.std(res, dtype=np.float64)
-print("Average: %s Standard deviation: %s"  % (avg, std))
+
+#avg = np.mean(res, dtype=np.float64)
+#std = np.std(res, dtype=np.float64)
+#res = res - avg
+#avg = np.mean(res, dtype=np.float64)
+#res = np.divide(res, std)
+#std = np.std(res, dtype=np.float64)
+#print("Average: %s Standard deviation: %s"  % (avg, std))
+
+labels = load.getLabels()
+data = tf.data.Dataset.from_tensor_slices((labels))
+data = data.map(load.loadImage2)
+iterator = data.make_initializable_iterator()
+next_element = iterator.get_next()
+
+with tf.Session() as sess:
+    sess.run(iterator.initializer)
+    elem = next_element.eval()
+    #Image.fromarray(np.asarray(elem)).show()
+    avg = np.mean(elem, dtype=np.float64)
+    std = np.std(elem, dtype=np.float64)
+    print("Average: %s Standard deviation: %s"  % (avg, std))
