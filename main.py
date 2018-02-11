476692f002210875c841977143150af21e9506ce
Viceriel <viceriel@gmail.com>
2018-02-11 09:44:08 +0100

    Standardisation of dataset prototype

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import load
 import pdb
+import numpy as np
 
 # plt.plot([1, 2, 3, 4])
 # plt.show()
-res = load.loadImage();
+res = load.loadImage()
+avg = np.mean(res, dtype=np.float64)
+std = np.std(res, dtype=np.float64)
+res = res - avg
+avg = np.mean(res, dtype=np.float64)
+res = np.divide(res, std)
+std = np.std(res, dtype=np.float64)
+print("Average: %s Standard deviation: %s"  % (avg, std))
