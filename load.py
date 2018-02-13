a8fe43b5b503bb1df0bd2b2d968c284cb5fb6246
Viceriel <viceriel@gmail.com>
2018-02-13 22:44:22 +0100

    Replace standardisation with normalisation, comments, flatting 3D image tensor

--- load.py
+++ load.py
@@ -2,13 +2,15 @@ import tensorflow as tf
 import numpy as np
 
 def loadImage2(img_path):
+    """Converts image at path to normalised tensor"""
     img_file = tf.read_file(img_path)
     img_decoded = tf.image.decode_image(img_file)
 
-    img_decoded= tf.image.per_image_standardization(img_decoded)
+    img_decoded = tf.reshape(tf.div(tf.to_float(img_decoded), 255), [-1])
     return img_decoded
 
 def getLabels():
+    """Returning array with names of jpeg images at images folder"""
     pattern = tf.train.match_filenames_once("./images/*.jpg")
     init_op = tf.local_variables_initializer()
 
--- main.py
+++ main.py
@@ -8,14 +8,6 @@ import pdb
 # plt.plot([1, 2, 3, 4])
 # plt.show()
 
-#avg = np.mean(res, dtype=np.float64)
-#std = np.std(res, dtype=np.float64)
-#res = res - avg
-#avg = np.mean(res, dtype=np.float64)
-#res = np.divide(res, std)
-#std = np.std(res, dtype=np.float64)
-#print("Average: %s Standard deviation: %s"  % (avg, std))
-
 labels = load.getLabels()
 data = tf.data.Dataset.from_tensor_slices((labels))
 data = data.map(load.loadImage2)
@@ -26,6 +18,4 @@ with tf.Session() as sess:
     sess.run(iterator.initializer)
     elem = next_element.eval()
     #Image.fromarray(np.asarray(elem)).show()
-    avg = np.mean(elem, dtype=np.float64)
-    std = np.std(elem, dtype=np.float64)
-    print("Average: %s Standard deviation: %s"  % (avg, std))
+    print(elem)
