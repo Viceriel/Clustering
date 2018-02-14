2b054e757776d8fd72bfc3071170c28a45c0cf92
Viceriel <viceriel@gmail.com>
2018-02-14 20:46:59 +0100

    Iterator in loop, real dataset load

--- load.py
+++ load.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
 import numpy as np
 
-def loadImage2(img_path):
+def loadImage(img_path):
     """Converts image at path to normalised tensor"""
     img_file = tf.read_file(img_path)
     img_decoded = tf.image.decode_image(img_file)
 
-    img_decoded = tf.reshape(tf.div(tf.to_float(img_decoded), 255), [-1])
+    img_decoded=tf.reshape(img_decoded, [512, 512])
+    #img_decoded = tf.reshape(tf.div(tf.to_float(img_decoded), 255), [-1])
     return img_decoded
 
-def getLabels():
+def getLabels(pattern):
     """Returning array with names of jpeg images at images folder"""
-    pattern = tf.train.match_filenames_once("./images/*.jpg")
+    pattern = tf.train.match_filenames_once(pattern)
     init_op = tf.local_variables_initializer()
 
     with tf.Session() as sess:
--- main.py
+++ main.py
@@ -8,14 +8,27 @@ import pdb
 # plt.plot([1, 2, 3, 4])
 # plt.show()
 
-labels = load.getLabels()
+real = input("Do you want use test(0) or real(1) dataset: ")
+
+if real == "1":
+    pattern= "./real/Jpeg8bit/*.jpg"
+else:
+    pattern= "./images/*.jpg"
+
+labels = load.getLabels(pattern)
+print(len(labels))
 data = tf.data.Dataset.from_tensor_slices((labels))
-data = data.map(load.loadImage2)
+data = data.map(load.loadImage)
 iterator = data.make_initializable_iterator()
 next_element = iterator.get_next()
 
 with tf.Session() as sess:
     sess.run(iterator.initializer)
-    elem = next_element.eval()
-    #Image.fromarray(np.asarray(elem)).show()
-    print(elem)
+    while True:
+        try:
+            elem = next_element.eval()
+            Image.fromarray(np.asarray(elem)).show()
+            next_element = iterator.get_next()
+        except tf.errors.OutOfRangeError:
+            break
+    #print(np.cov(elem))
